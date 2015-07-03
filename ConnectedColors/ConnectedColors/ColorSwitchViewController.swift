2a84415cd1eef8cab6c02ee5e5417c391e716e11
Philipp Eiglsperger <Philipp@dhcp-10-176-84-49.dynamic.eduroam.mwn.de>
2015-07-03 13:06:06 +0200

    Lower quality

--- ConnectedColors/ConnectedColors/ColorSwitchViewController.swift
+++ ConnectedColors/ConnectedColors/ColorSwitchViewController.swift
@@ -235,11 +235,11 @@ class ColorSwitchViewController: UIViewController {
         }
         
         var recordSettings:[NSObject: AnyObject] = [
-            AVFormatIDKey: kAudioFormatAppleLossless,
-            AVEncoderAudioQualityKey : AVAudioQuality.Max.rawValue,
-            AVEncoderBitRateKey : 320000,
-            AVNumberOfChannelsKey: 2,
-            AVSampleRateKey : 44100.0
+                AVFormatIDKey: kAudioFormatMPEG4AAC,
+                AVEncoderAudioQualityKey : AVAudioQuality.Low.rawValue,
+                AVEncoderBitRateKey : 4800,
+                AVNumberOfChannelsKey: 1,
+                AVSampleRateKey : 22100.0
         ]
         var error: NSError?
         recorder = AVAudioRecorder(URL: soundFileURL!, settings: recordSettings, error: &error)
